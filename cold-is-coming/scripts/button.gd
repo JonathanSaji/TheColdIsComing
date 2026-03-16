7d07417d6dcc943dcc70367435e7efbab4942089
Jonathan Saji <jonathansaji2009@gmail.com>
2026-03-16 12:00:55 -0400

    added sound effects

--- cold-is-coming/scripts/button.gd
+++ cold-is-coming/scripts/button.gd
@@ -1,6 +1,9 @@
 extends Button
 
 @onready var highlight = $NinePatchRect  # your overlay node
+@onready var selection = get_node("/root/Main Menu/Effects/selection") #audio stream player
+@onready var clicked = get_node("/root/Main Menu/Effects/clicked") #audio stream player
+
 var tween: Tween
 
 func _ready() -> void:
@@ -12,9 +15,14 @@ func _on_mouse_entered():
 		tween.kill()
 	tween = create_tween()
 	tween.tween_property(highlight, "modulate:a", 1.0, 0.3)  # 0.3 = speed in seconds
+	selection.play()
 
 func _on_mouse_exited():
 	if tween:
 		tween.kill()
 	tween = create_tween()
 	tween.tween_property(highlight, "modulate:a", 0.0, 0.3)
+
+func _on_pressed():
+	clicked.play()
+ 
